913ee9fcbf9dd0bc91f060ee6f92dcbf522d476f
Isaias Santos <isaias.santos@boldcommerce.com>
2025-06-29 11:46:53 -0300

    feat(decks): refactor Decks module structure; remove old controller and service, add new controller tests, and update module imports

--- packages/backendFlashCards/src/app/app.module.ts
+++ packages/backendFlashCards/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DecksModule } from '../decks/decks.module';
-
-/**
- * NestJS: opnetive fremawork
- * controller: routes and request handling
- * service: business logic and data handling
- * module: encapsulates controllers and providers
- *  */
+import { DecksModule } from '../modules/decks/decks.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Deck } from '../modules/decks/decks.entity';
+import { User } from '../modules/users/infrastructure/user.entity';
+import { Card } from '../modules/cards/cards.entity';
 
 @Module({
-  imports: [DecksModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+      host: process.env.DATABASE_URL,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      logging: true,
+      models: [Deck, User, Card], // Add your models here
+    }),
+    DecksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/backendFlashCards/src/decks/decks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DecksService } from './decks.service';
-import { DecksController } from './decks.controller';
-
-@Module({
-  providers: [DecksService],
-  controllers: [DecksController]
-})
-export class DecksModule {}
